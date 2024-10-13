528a054c97bae3113b6b39b50db98722a2e8378e
Harrison <harrison.d.martin@gmail.com>
2024-10-13 16:08:32 -0600

    updated package.json to have homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HashRouter  as Router, Route, Routes, Link } from 'react-router-dom';
 import ProjectDetails from './Components/ProjectDetails/ProjectDetails';
 import Homepage from './Components/Homepage/Homepage';
 
