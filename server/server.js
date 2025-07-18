a0239313016b983f000fa1efb4e73a5ca117fbaf
Harrison <harrison.d.martin@gmail.com>
2025-07-18 09:06:07 -0600

    revert cors (mistake)

--- server/server.js
+++ server/server.js
@@ -33,12 +33,7 @@ client.once('ready', () => {
 // --- END DISCORD BOT SETUP ---
 
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-  credentials: false
-}));
+app.use(cors());
 app.use(express.json());
 app.use(useragent.express()); // Enable express-useragent
 
