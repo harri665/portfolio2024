e8cb46e6df2e0b5b87efd5df7ea7d418434b7e2a
Harrison <harrison.d.martin@gmail.com>
2025-07-18 09:04:59 -0600

    all cors

--- server/server.js
+++ server/server.js
@@ -33,7 +33,12 @@ client.once('ready', () => {
 // --- END DISCORD BOT SETUP ---
 
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  credentials: false
+}));
 app.use(express.json());
 app.use(useragent.express()); // Enable express-useragent
 
